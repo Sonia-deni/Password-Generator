09c84e60d4535a790189279006d60bbf7bf8bb0e
sonia-deni <sonia_deni@hotmail.com>
2023-01-06 20:09:46 +0000

    added code to create an object to store user options for the password once the generate password button is clicked. Prompts and confirms to collect user inputs, store these in the object and then return the object from the function

--- script.js
+++ script.js
@@ -90,8 +90,13 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+  var passwordLength = prompt("How many characters would you like for your password? Please choose between 10 and 64");
+  var lowercaseLetters = confirm("Would you like lowercase letters?");
+  var uppercaseLetters = confirm("Would you like uppercase letters?");
+  var numberCharacters = confirm("would you like numbers?");
+  var specialCharacters = confirm("Would you like special characters?");
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -101,7 +106,16 @@ function getRandom(arr) {
 // Function to generate password with user input
 function generatePassword() {
 
-}
+    var options = {}
+      options["passwordLength"] = prompt("How many characters would you like for your password? Please choose between 10 and 64");
+      options["lowercaseLetters"]= confirm("Would you like lowercase letters?");
+      options["uppercaseLetters"]= confirm("Would you like uppercase letters?");
+      options["numberCharacters"]= confirm("would you like numbers?");
+      options["specialCharacters"]= confirm("Would you like special characters?");
+      console.log(options);
+      return options;
+  }
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -116,3 +130,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+
+//when the user clicks the button
+  //prompt for length
+  //check valid, if so, store. If not, re prompt
+  //next prompt for character types
+  //store boolean true/false
+
+//create new array for password characters to choose from
+//if character type == true, add this array to password array
+//do this for each character type
+//change passwordlength from a string to a number using parseInt()
+//loop over the new long array for the length of the password, and each time randomly select a character. Push this into a new array and return it
